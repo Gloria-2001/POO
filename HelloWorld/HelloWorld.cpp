2096a363fa0c6717a3b013b5e6b0e574efe86cc9
Zait06 <zait-hdez@hotmail.com>
2020-06-22 17:14:22 -0500

    Hello cpp

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 int main(){
-    cout<<"Hello World!!"<<endl;
+    char a;
+    cout<<"Hello World!!"<<endl<<"Ingrese un numero: ";
+    cin>>a;
+    cout<<"Print number: "<<a<<endl;
     return 0;
 }
