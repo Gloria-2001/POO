935b0e53bd71fd860c2faed7caaf11adc3004132
Zait06 <zait-hdez@hotmail.com>
2020-08-30 18:29:17 -0500

    Actualizacion

---
+++ InterfazUsuario/05-ObtenerDatos/Maquina.java
@@ -0,0 +1,67 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Maquina extends JFrame implements ActionListener{
+    
+    private JButton perro;
+    private JButton gato;
+    private JButton huron;
+    private Container cp;
+    private JLabel animal;
+
+    public Maquina(){
+        super("Maquina");
+        setSize(600,300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        cp = getContentPane();
+        cp.setLayout(new FlowLayout());
+
+        perro = new JButton("");
+        gato = new JButton("");
+        huron = new JButton("");
+        animal = new JLabel("______________________");
+
+        init();
+    }
+
+    private void init(){
+        perro.setIcon(new ImageIcon("perro.jpg"));
+        perro.addActionListener(this);
+        cp.add(perro);
+
+        gato.setIcon(new ImageIcon("gato.jpg"));
+        gato.addActionListener(this);
+        cp.add(gato);
+
+        huron.setIcon(new ImageIcon("huron.jpg"));
+        huron.addActionListener(this);
+        cp.add(huron);
+
+        cp.add(animal);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        ImageIcon aux;
+        animal.setText("");
+        if(e.getSource() == perro){
+            aux = new ImageIcon("perro.jpg");
+        }else if(e.getSource() == gato){
+            aux = new ImageIcon("gato.jpg");
+        }else{
+            aux = new ImageIcon("huron.jpg");
+        }
+        animal.setIcon(aux);
+    }
+
+    public void run(){
+        setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        Maquina m = new Maquina();
+        m.run();
+    }   
+}
---
+++ InterfazUsuario/05-ObtenerDatos/MaquinaArray.java
@@ -0,0 +1,63 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MaquinaArray extends JFrame implements ActionListener{
+
+    private JButton animales[];
+    private JLabel animal;
+    private Container cp;
+    private String []img = {
+        "perro.jpg",
+        "gato.jpg",
+        "huron.jpg"
+    };
+
+    public MaquinaArray(){
+        super("Maquina Array");
+        setSize(600,300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        cp = getContentPane();
+        cp.setLayout(new FlowLayout());
+        
+        animales = new JButton[3];
+        animal = new JLabel("______________");
+
+        init();
+    }
+
+    private void init(){
+        for(int i=0; i<3; i++){
+            JButton aux = new JButton();
+            aux.setIcon(new ImageIcon(img[i]));
+            aux.addActionListener(this);
+            cp.add(aux);
+            animales[i] = aux;
+        }
+
+        cp.add(animal);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        ImageIcon aux;
+        for(int i=0; i<3; i++){
+            if(e.getSource() == animales[i]){
+                aux = new ImageIcon(img[i]);
+                animal.setIcon(aux);
+                break;
+            }
+        }
+        animal.setText("");
+    }
+
+    public void run(){
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MaquinaArray m = new MaquinaArray();
+        m.run();
+    }
+}
