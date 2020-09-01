1eeb94df5d0092a270b259dc27251ebe4ecee010
Zait06 <zait-hdez@hotmail.com>
2020-09-01 17:50:28 -0500

    Radio Button

--- InterfazUsuario/07-RadioButton/Radio00.java
+++ InterfazUsuario/07-RadioButton/Radio00.java
@@ -2,13 +2,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Radio00 extends JFrame{
+public class Radio00 extends JFrame implements ActionListener{
 
     private JRadioButton perro;
     private JRadioButton gato;
     private JRadioButton huron;
     private Container cp;
     private ButtonGroup bg;
+    private JButton ac;
 
     public Radio00(){
         super("Radio Button");
@@ -21,6 +22,7 @@ public class Radio00 extends JFrame{
         perro = new JRadioButton("perro");
         gato = new JRadioButton("gato");
         huron = new JRadioButton("huron");
+        ac = new JButton("Mostrar");
 
         bg = new ButtonGroup();
 
@@ -32,9 +34,27 @@ public class Radio00 extends JFrame{
         bg.add(gato);
         bg.add(huron);
 
+        // perro.addActionListener(this);
+        // gato.addActionListener(this);
+        // huron.addActionListener(this);
+
         cp.add(perro);
         cp.add(gato);
         cp.add(huron);
+
+        ac.addActionListener(this);
+        cp.add(ac);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        if(perro.isSelected()){
+            System.out.println("perro");
+        }else if(gato.isSelected()){
+            System.out.println("gato");
+        }else if(huron.isSelected()){
+            System.out.println("huron");
+        }
     }
 
     public void run(){
---
+++ InterfazUsuario/07-RadioButton/Radio01.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Radio01 extends JFrame implements ActionListener{
+
+    private JRadioButton []radios;
+    private Container cp;
+    private ButtonGroup bg;
+    private JLabel ac;
+    private String animal[] = {"perro", "gato", "huron"};
+
+    public Radio01(){
+        super("Radio Button");
+        setSize(600,300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        cp = getContentPane();
+        cp.setLayout(new FlowLayout());
+
+        radios = new JRadioButton[3];
+        ac = new JLabel("_____Mostrar_____");
+
+        bg = new ButtonGroup();
+
+        init();
+    }
+
+    private void init(){
+        for(int i=0; i<3; i++){
+            radios[i] = new JRadioButton(animal[i]);
+            radios[i].addActionListener(this);
+            bg.add(radios[i]);
+            cp.add(radios[i]);
+        }
+
+        cp.add(ac);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        ac.setText("");
+        ac.setIcon(new ImageIcon("../img/" + e.getActionCommand() + ".jpg"));
+    }
+
+    public void run(){
+        setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        Radio01 r =  new Radio01();
+        r.run();
+    }
+}
